f30b84426f5229b33c07e8f4952e26cdf938c591
tmwebmath <tmwebmath@gmail.com>
2014-11-26 18:07:20 +0000

    Ajoute les modeles Teacher et Class

--- teachers/models.py
+++ teachers/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from students.models import Student
 
 class Teacher(User):
     e_mail = models.CharField(max_length=30)
+
+class Class(models.Model):
+    name = models.Charfield(max_length=30)
+    teacher = models.ManyToManyField(Teacher)
+    student = models.ManyToManyField(Student)
