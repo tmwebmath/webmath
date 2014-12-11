7f5875121da464ec2547a3d651ab720a1c91d6fe
khcr <kocher.ke@gmail.com>
2014-12-11 17:12:15 +0100

    restless api

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from courses import views
+from courses.api import CourseList, CourseDetail, PageCourseDetail, ThemeList
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^api/courses$', CourseList.as_view()),
+    url(r'^api/courses/(?P<pk>\d+)$', CourseDetail.as_view()),
+    url(r'^api/pages/(?P<page_id>\d+)/courses/(?P<course_id>\d+)$', PageCourseDetail.as_view()),
+    url(r'^api/themes$', ThemeList.as_view()),
 )
